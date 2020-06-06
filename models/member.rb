125dd82116dd58817db016b214dbf4b8d5313fd7
Ed <edward.alexander.cant@gmail.com>
2020-06-06 11:23:43 +0100

    models: implement CRUD functionality for the member object

--- models/member.rb
+++ models/member.rb
@@ -22,4 +22,25 @@ class Member
         values = [@name, @dob, @pin]
         @id = SqlRunner.run(sql, values)[0]['id'].to_i()
     end
+
+    def delete()
+        sql = "DELETE FROM members
+                WHERE id = $1;"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
+    def self.delete_all
+        sql = "DELETE FROM members;"
+        SqlRunner.run(sql)
+    end
+
+    def update()
+        sql = "UPDATE members
+                SET (name, dob, pin)
+                = ($2, $3, $4)
+                WHERE id = $1"
+        values = [@id, @name, @dob, @pin]
+        SqlRunner.run(sql, values)
+    end
 end
