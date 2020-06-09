17ed32ac3ef08ae00c6a751fff278e63e861225e
Ed <edward.alexander.cant@gmail.com>
2020-06-09 09:59:55 +0100

    have the web app display the members taking a class

--- controllers/class.rb
+++ controllers/class.rb
@@ -7,5 +7,6 @@ require_relative('../models/booking')
 
 get '/class/:class' do
     @class = GymClass.by_id(params[:class])
+    @members = Booking.members_taking_class(@class.id)
     erb( :'class' )
 end
