264c257d136da8bdf733339cf09415ce5c2e7862
ed-commits <63356087+ed-commits@users.noreply.github.com>
2020-06-21 18:12:01 +0100

    Fix XSS vulnerabilities by escaping output. * Use <%== %> instead of <%= %> in a HTML Element Content context. * Use <%=attr_esc %> in an HTML attribute context.
    References:
    * https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require( 'erubis' )
 
 require_relative('controllers/signup')
 require_relative('controllers/classes')
 require_relative('controllers/class')
 require_relative('controllers/terminate')
 require_relative('controllers/booking')
+
+helpers do
+    def attr_esc(text)
+      Rack::Utils.escape_path(text)
+    end
+  end
 
 get '/' do
     erb( :index )
